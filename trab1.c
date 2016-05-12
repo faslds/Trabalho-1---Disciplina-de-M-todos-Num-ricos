7baad6e98c9c17c927b89efc9aa45bd986454305
faslds <felipe.senger.souza@usp.br>
2016-05-12 19:54:02 -0300

    v1.2

--- trab1.c
+++ trab1.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include<math.h>
+#define T 666
+#define M 0,0000001
 
 double *createmat(int n)
 {
@@ -64,19 +67,61 @@ double *fillb(n)
 	return b;
 }
 
-void gaussseidel(double *mat, double *b, double* x)
+void sor(double w, double *mat, double *b, double* x, double *xold)
 {
+	//double a = 0.0;
 	int i=0, j=0;
 	for(i=0; i<n; i++)
 	{
+		xold[i] = x[i];
+	}
+	for(i=0; i<n; i++)
+	{
+		a = 0.0;
 		for(j=0; j<n; j++)
 		{
 			if(j != i)
 			{
-				a = a - mat[i][j]*x[j];
+				a = a + mat[i][j]*x[j];
 			}
 		}
-		x[i] = w*(b[j] - a);
+		x[i] = w*(b[i] - a)/mat[i][i];
+	}
+	for(i=0; i<n; i++)
+	{
+		x[i] = x[i] - (w-1)*xold[i];
 	}
 }
 
+double erro(double *x, double *xold)
+{
+	int = i;
+	double e = 0.0;
+	for(i=0; i<n; i++)
+	{
+		e = e + ((x[i] - xold[i])*(x[i] - xold[i]));
+	}
+	e = sqrt(e);
+	return e;
+}
+
+ int main()
+ {
+ 	int i=0, k=0;
+ 	double x[n], b[n], 
+ 	//for abaixo eh para os diferentes w
+ 	for(i=0; i<=10; i++)
+ 	{
+ 		//cria o vet k = 0
+		for()
+	 	while(k < T || e >= M)
+	 	{
+	 		sor(w, mat, b, x, xold);
+	 		erro(x, xold);
+	 		k++;
+		}
+		w = w + 0.1;
+		
+	}
+	
+ }
