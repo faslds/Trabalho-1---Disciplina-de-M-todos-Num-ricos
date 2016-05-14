42b491285614e6ba125fc013ed94cfc0e9babf7e
faslds <felipe.senger.souza@usp.br>
2016-05-14 14:49:53 -0300

    v2.5
    Codigo organizado e comentado

--- trab1.c
+++ trab1.c
@@ -4,60 +4,60 @@
 
 void sor(double **mat, double *b, double emax, double w, int t)
 {
-	int i, j, k=0, N=t/5;
+	int i, j, k=0, N=t/5; /*N eh a ordem da matriz e recebe t/5, onde 
+	t eh a quantidade 	maxima de 	iteracoes, definida como t = n*5*/
 	double a, e=1.0;
-	double *x = malloc(N*sizeof(double));
+	double *x = malloc(N*sizeof(double)); //aloca o espaco necessario para os vetores da iteracao x(k) e x(k+1)
 	double *xold = malloc(N*sizeof(double));	
-	//for(j=0; j<=20; j++)
-	//{	
-		for(i = 0; i<N; i++)
+	//esse for preenche a aproximacao inicial
+	for(i = 0; i<N; i++)
+	{
+		x[i] = 1; //nesse caso todos os elementos sao igual a 1
+	}
+	e = 1.0;
+	while((k<=t-1)&&(e>=emax))
+	{	
+		int i=0, j=0;
+		for(i=0; i<N; i++) //copia a iteracao atual na velha
 		{
-			x[i] = 1;
+			xold[i] = x[i];
 		}
-		e = 1.0;
-		while((k<=t-1)&&(e>=emax))
-		{	
-			int i=0, j=0;
-			for(i=0; i<N; i++)
+		for(i=0; i<N; i++) //trabalha na iteracao atual
+		{
+			a = 0.0;
+			for(j=0; j<N; j++)
 			{
-				xold[i] = x[i];
-			}
-			for(i=0; i<N; i++)
-			{
-				a = 0.0;
-				for(j=0; j<N; j++)
+				if(j != i)
 				{
-					if(j != i)
-					{
-						a = a + (mat[i][j])*(x[j]);
-					}
+					a = a + (mat[i][j])*(x[j]);
 				}
-			x[i] = w*(b[i] - a)/mat[i][i];
-			x[i] = x[i] - (w-1)*xold[i];
 			}
-			e = 0.0;
-				for(i=0; i<N; i++)
-				{
-					e = e + ((x[i] - xold[i])*(x[i] - xold[i]));
-				}
-				e = sqrt(e);
-				k++;
+		//aqui o metodo SOR eh aplicado	
+		x[i] = w*(b[i] - a)/mat[i][i];
+		x[i] = x[i] - (w-1)*xold[i];
 		}
-		if(e <= emax)
+		e = 0.0; 
+			for(i=0; i<N; i++) //calcula o erro
+			{
+				e = e + ((x[i] - xold[i])*(x[i] - xold[i]));
+			}
+			e = sqrt(e); 
+			k++;
+	}
+	if(e <= emax) //se o erro eh menor que a precisao requerida, o metodo converge
+ 		{
+			printf("\n\nO sistema converge para w = %.2f e sua solucao eh:\n\n", w);
+	 		for(i=0; i<N; i++) //imprime a solucao
 	 		{
-				printf("\n\nO sistema converge para w = %.2f e sua solucao eh:\n\n", w);
-		 		for(i=0; i<N; i++)
-		 		{
-		 			printf("%f\n", x[i]);
-				}	
-				printf("\nNumero de iteracoes: %d\n\n", k);
-			}
-			else if(k = t)
-			{
-				printf("\nO sistema diverge para w = %.2f ou nao converge em menos de %d iteracoes\n", w, t);
-			}
-	 	k = 0;
-	//}
+	 			printf("%f\n", x[i]);
+			}	
+			printf("\nNumero de iteracoes: %d\n\n", k);
+		}
+		else if(k = t) //se alcancou o numero maximo de iteraçoes, o metodo nao convergiu para a solucao do sistema
+		{
+			printf("\nO sistema diverge para w = %.2f ou nao converge em menos de %d iteracoes\n", w, t);
+		}
+ 	k = 0; //zera o contador de iteracoes
 }
 
 int main()
@@ -66,23 +66,21 @@ int main()
  	double emax;
  	double **mat, *b;
 	double e = 1.0;
- 	double w = 0.1;
- 
-	printf("Ola, este programa ira resolver um sistema linear pelo metodo SOR Ax = b\n");
+ 	double w = 0.0;
+ 	printf("Ola, este programa ira resolver um sistema linear pelo metodo SOR Ax = b\n");
  	printf("Qual sera a dimensao da matriz A? ");
  	scanf("%d", &N);
  	printf("Quantas casas decimais de precisao voce deseja?");
  	scanf("%d", &p);
- 	emax = pow(0.1, p);
- 	printf("\n\nprecisao usada: %f\n\n", emax);
- 	mat = malloc(N*sizeof(double *));
+ 	emax = pow(0.1, p); //calcula a precisao pedida
+ 	mat = malloc(N*sizeof(double *));//aloca o espaco para a matriz A (chamada de mat)
  	for(i=0; i<N; i++)
  	{
- 		mat[i] = malloc(N*sizeof(double));
+ 		mat[i] = malloc(N*sizeof(double));//mat eh estruturada como um vetor de ponteiros para vetores
 	}
-	b = malloc(N*sizeof(double));
-	t = 5*N;
-	for (i=0; i<N; i++)
+	b = malloc(N*sizeof(double));//aloca o espaco para a matriz b
+	t = 5*N;// t eh o numero maximo aceito de iteracoes
+	for (i=0; i<N; i++)//esse for cria a matriz mat
 	{
 		for (j=0; j<N; j++)
 		{
@@ -124,12 +122,12 @@ int main()
 	mat[0][1] = 5.5;
 	mat[1][0] = 5.5;
 
-	for(i = 0; i < N; i++)
+	for(i = 0; i < N; i++)//preenche a matriz b
 	{
 		b[i] = 1.0/(i+1);
 	}
  	printf("Sua matriz A eh:\n\n");
-	for(i=0; i<N; i++)
+	for(i=0; i<N; i++)//fornece a matriz mat impressa
 	{
 		for(j=0; j<N; j++)
 		{
@@ -137,13 +135,14 @@ int main()
 		}
 			printf("\n");	
 	}
-	printf("\nSua matriz b eh:\n\n");
+	printf("\nSua matriz b eh:\n\n");//fornece a matriz b impressa
 	for(i = 0; i < N; i++)
 	{
-		printf("b[%d] = %f\n", i, b[i]);
+		printf("b[%d] = %f\n", i+1, b[i]);
 	}
 		e = 1.0;
-		for(j=0; j<=19; j++)
+		//chama a funcao sor para as 20 diferentes aplicacoes para w variando de 0 a 2
+		for(j=0; j<=20; j++)
 		{
 			sor(mat, b, emax, w, t);
 			w = w + 0.1;
